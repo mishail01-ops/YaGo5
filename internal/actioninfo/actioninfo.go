ad8c78a35618e4a9eb8883daaf83570df73cb572
mishail01-ops <mishail01@gmail.com>
2026-02-18 14:53:06 +0300

    0.09

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -13,25 +13,48 @@ type DataParser interface {
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
-	var err error
-	var str string
-	err = nil
-	for _, value := range dataset {
-		err = nil
-		err = dp.Parse(value)
+	//var err error
+	//var str string
+	//err = nil
+	//for _, value := range dataset {
+	//	err = nil
+	//	err = dp.Parse(value)
 
-		if err != nil {
+	//	if err != nil {
+	//		log.Println(err)
+	//	}
+	//}
+
+	//err = nil
+
+	//str, err = dp.ActionInfo()
+
+	//if err != nil {
+	//	log.Println(err)
+	//}
+
+	//fmt.Print(str)
+
+	parsedOK := false
+
+	for _, value := range dataset {
+		if err := dp.Parse(value); err != nil {
 			log.Println(err)
+			continue
 		}
+		parsedOK = true
 	}
 
-	err = nil
+	if !parsedOK {
+		return
+	}
 
-	str, err = dp.ActionInfo()
-
+	str, err := dp.ActionInfo()
 	if err != nil {
 		log.Println(err)
+		return
 	}
 
 	fmt.Print(str)
+
 }
